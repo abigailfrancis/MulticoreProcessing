fd5af7b9723e1569fe134650ca2241b23c8643ae
ccowden <catie.cowden@gmail.com>
2020-09-07 13:21:03 -0700

    Initial attempt Q4

--- src/q5/Frequency.java
+++ src/q5/Frequency.java
@@ -1,15 +1,103 @@
 package q5;
 
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Frequency implements Callable<Integer> {
-    public static int parallelFreq(int x, int[] A, int numThreads){
-        //your implementation goes here, return -1 if the input is not valid.
-        return -1;
+
+    private final int target;
+    private final int[] subArray;
+
+    /*
+    Creates a new instance of the Frequency class
+     */
+    public Frequency(int target, int[] subArray) {
+        this.target = target;
+        this.subArray = subArray;
     }
 
     @Override
-    public Integer call() throws Exception {
-        return null;
+    public Integer call() {
+        Integer total = 0;
+        for(int i = 0; i < this.subArray.length; i++) {
+            if (this.subArray[i] == this.target) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    /*
+    Creates numThreads threads that will compute the frequency of integer x within Array A
+     */
+    public static int parallelFreq(int x, int[] A, int numThreads){
+
+        // Set up
+        int result = 0;
+        int start = 0;
+        int end = 0;
+        int[] subArr = {};
+
+        // Input validation
+        // Check if numThreads is an invalid number
+        if (numThreads <= 0) {
+            return -1;
+        }
+
+        // Check if array is null or empty
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+
+        // Check if we're being asked to generate more threads than the size of the array
+        if (numThreads > A.length) {
+            // If we've been asked to divide the array into too many pieces
+            numThreads = A.length;
+        }
+
+        int interval = A.length / numThreads;
+
+        // Create ExecutorService with numThreads threads
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        // Create a list of Frequency callables
+        List<Frequency> callables = new ArrayList<>();
+
+        for(int i = 0; i < numThreads; i++){
+            start = i * interval;
+            end = Math.min(start + interval, A.length);
+
+            subArr = Arrays.copyOfRange(A, start, end);
+
+            Frequency f = new Frequency(x, subArr);
+            callables.add(f);
+        }
+
+        // Create a list of Futures to hold the results of the Callables
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        // Invoke all of the Callables
+        try {
+            futures  = executor.invokeAll(callables);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Examine the results
+        for(Future<Integer> fut : futures){
+            try {
+                result += fut.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Shut down the executor service now
+        executor.shutdown();
+
+        // Return the total computed frequency
+        return result;
     }
 }
