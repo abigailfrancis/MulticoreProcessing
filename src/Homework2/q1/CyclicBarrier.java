752844a9ae6adc4ac9fa669cdcf116df7b1bb4e5
ccowden <catie.cowden@gmail.com>
2020-10-03 22:58:26 -0700

    Adding method templates

--- src/Homework2/q1/CyclicBarrier.java
+++ src/Homework2/q1/CyclicBarrier.java
@@ -1,4 +1,21 @@
 package Homework2.q1;
 
 public class CyclicBarrier {
+    public CyclicBarrier(int parties) {
+        // Creates a new CyclicBarrier that will release threads only when
+        // the given number of threads are waiting upon it
+    }
+
+    int await() throws InterruptedException {
+        // Waits until all parties have invoked await on this CyclicBarrier.
+        // If the current thread is not the last to arrive then it is
+        // disabled for thread scheduling purposes and lies dormant until
+        // the last thread arrives.
+        // Returns: the arrival index of the current thread, where index
+        // (parties - 1) indicates the first to arrive and zero indicates
+        // the last to arrive.
+
+        return 0;
+    }
 }
+
